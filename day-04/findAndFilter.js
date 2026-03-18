cab607696ec17cc897cc7c79f711be7deb5bdfe6
jbjzeehad <ilamta.97@gmail.com>
2026-03-19 03:47:20 +0600

    ok

---
+++ day-04/findAndFilter.js
@@ -0,0 +1,25 @@
+/**find() হলো তীরের মতো যা একটা লক্ষ্যভেদ করেই থেমে যায়, আর filter() হলো ছাঁকনির মতো যা পুরো মিশ্রণ থেকে নির্দিষ্ট জিনিসের একটা সংগ্রহ বের করে আনে। */
+
+const books = [
+  { id: 100, title: "Himu", author: "Humayun Ahmed" },
+  { id: 101, title: "Misir Ali", author: "Humayun Ahmed" },
+  { id: 102, title: "Pather Panchali", author: "Bibhutibhushan" },
+];
+
+const myBook = books.find((book) => book.id === 101);
+
+console.log(myBook);
+
+// find() : "নির্দিষ্ট একজনকে খোঁজা" শর্ত মিললে সাথে সাথে কাজ থামিয়ে দেয়। এটি সরাসরি একটি Object দেয়। মূল উদ্দেশ্য	ইউনিক কিছু বের করা (যেমন: ID, Email)। আউটপুট	একটি সিঙ্গেল উপাদান (Element/Object)।
+
+const books3 = [
+  { id: 100, title: "Himu", author: "Humayun Ahmed" },
+  { id: 101, title: "Misir Ali", author: "Humayun Ahmed" },
+  { id: 102, title: "Pather Panchali", author: "Bibhutibhushan" },
+];
+
+const humayunBooks = books3.filter((book) => book.author === "Humayun Ahmed");
+
+console.log(humayunBooks);
+
+// পুরো অ্যারের শেষ পর্যন্ত চেক করে। মূল উদ্দেশ্য ফিল্টার করে ছোট লিস্ট বানানো (যেমন: Active Users)। আউটপুট একটি নতুন অ্যারে (Array)।
